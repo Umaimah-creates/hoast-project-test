1112959a1633cd59f5d2af6f70f09d80340b1cd5
Umaimah-creates <umaimaharif47@gmail.com>
2025-09-16 02:58:27 -0700

    Update page.tsx

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -15,7 +15,7 @@ export default function Contact() {
     const data = new FormData(form);
 
     try {
-      const res = await fetch("https://formspree.io/f/YOUR_FORM_ID", {
+      const res = await fetch("https://formspree.io/f/mldwvepe", {
         method: "POST",
         body: data,
         headers: { Accept: "application/json" },
